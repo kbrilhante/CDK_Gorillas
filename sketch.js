7ff59caf96807dac4fc2bb845345c4777de45460
Karina <kbrilhante@gmail.com>
2023-09-29 23:55:39 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -8,22 +8,22 @@ const properties = {
 let canvas;
 let buildings = [];
 
-function preload() { }
+function preload() {}
 
 function setup() {
     canvas = createCanvas(800, 600);
 
     let buildingX = 0;
     let buildingW = 0;
-    do {
+    while (buildingX < width - properties.tileSize * properties.columnWidth * 3) {
         const col = floor(random(properties.columns[0], properties.columns[1] + 1)) * properties.columnWidth * 3;
         const fl = floor(random(properties.floors[0], properties.floors[1] + 1)) * properties.columnWidth * 4;
-        let building = new Building(buildingX, col, fl, properties.tileSize, properties.columnWidth);
+        let building = new Building (buildingX, col, fl, properties.tileSize, properties.columnWidth);
         buildingW = col * properties.tileSize;
         buildingX += buildingW + properties.gap;
         buildings.push(building);
-    } while (buildingX < width - properties.tileSize * properties.columnWidth * 3)
-
+    } 
+    
 }
 
 function draw() {

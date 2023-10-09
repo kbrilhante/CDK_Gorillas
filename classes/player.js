9b0f4a135d5d3b7de034a11e980394dbeb5b81d6
Karina <kbrilhante@gmail.com>
2023-10-08 23:10:59 -0300

    bunny being a weirdo like Dingo

--- classes/player.js
+++ classes/player.js
@@ -1,5 +1,6 @@
 class Player {
     constructor(index, buildings) {
+        this.ani = sprites.Idle;
         this.index = index;
         let i;
         if (this.index == 0) {
@@ -7,20 +8,29 @@ class Player {
         } else {
             i = buildings.length - 2; 
         }
-        this.radius = 15;
+        this.radius = 20;
         this.pos = buildings[i].getTop();
-        this.pos.y -= this.radius;
+        this.scale = this.radius * 2 / this.ani[0].width;
         this.collided = false;
         this.score = 0;
+        this.frameCount = 0;
+        this.frame = this.ani[this.frameCount];
+        this.frameRate = 0.6;
+        this.w = this.frame.width * this.scale;
+        this.h = this.w * this.frame.height / this.frame.width;
+        this.pos.y -= this.h / 2;
     }
     display() {
         if (!this.collided) {
             push();
-            ellipseMode(RADIUS);
-            fill("green");
-            noStroke();
-            circle(this.pos.x, this.pos.y, this.radius);
+            // ellipseMode(RADIUS);
+            // fill("green");
+            // noStroke();
+            // circle(this.pos.x, this.pos.y, this.radius);
+            imageMode(CENTER);
+            image(this.frame, this.pos.x, this.pos.y, this.w, this.h)
             pop();
+            this.animate();
         }
     }
     collision(buildings) {
@@ -29,5 +39,11 @@ class Player {
             const b = buildings[i];
             b.collision(this.pos.x, this.pos.y, this.radius * 6);
         }
+    }
+    changeAnimation(newAnimation) {}
+    animate() {
+        this.frameCount += this.frameRate;
+        const i = floor (this.frameCount % this.ani.length);
+        this.frame = this.ani[i];
     }
 }
